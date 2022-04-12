b363582c1587257f3d68846d4616dc92b5cacc9d
ErikBoesen <me@erikboesen.com>
2022-04-12 18:49:26 -0400

    Add initial code for bot

---
+++ bot.py
@@ -0,0 +1,45 @@
+import os
+import requests
+import mebots
+import yalies
+import datetime
+import json
+
+
+with open('config.json', 'r') as f:
+    config = json.load(f)
+bot = mebots.Bot('butteur_bot', config['mebots_token'])
+yalies_api = yalies.API(config['yalies_api_key'])
+
+
+def get_students():
+    date = datetime.date.today()
+    print({'college_code': 'GH', 'birth_month': date.month, 'birth_day': date.day - 1})
+    students = yalies_api.people(filters={'college_code': 'GH', 'birth_month': date.month, 'birth_day': date.day - 1})
+    return [student.first_name + ' ' + student.last_name for student in students]
+
+
+def send(text, bot_id):
+    url  = 'https://api.groupme.com/v3/bots/post'
+
+    message = {
+        'bot_id': bot_id,
+        'text': text,
+    }
+    r = requests.post(url, json=message)
+
+students = get_students()
+message = None
+if students:
+    message = 'Happy birthday to '
+    if len(students) > 1:
+        last = students.pop()
+        message += ', '.join(students)
+        message += ', & ' + last
+    else:
+        message += students[0]
+    message += '!'
+
+    bot_ids = [instance.id for instance in bot.instances()]
+    for bot_id in bot_ids:
+        send(message, bot_id)
